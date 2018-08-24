9281ad5e4b78803f6aa6a8125cd24ceb5a5c333d
myricoi <myricoidixon@gmail.com>
2018-08-24 05:52:47 +0300

    setup coveralls

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask
+from StackOverflow_Lite.api.version_1.views import routes
 
 app = Flask(__name__)
+app.register_blueprint(routes.mod, url_prefix='/api/v1')
 if __name__ == '__main__':
-    from StackOverflow_Lite.api.version_1.views import routes
-    app.register_blueprint(routes.mod, url_prefix='/api/v1')
-    app.run()
+	app.run()
--- tests/test_views.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify, make_response
-from ..StackOverflow_Lite.api.version_1.views import routes
-from ..run import app
-
-
-def test_it_works():
-    pass
